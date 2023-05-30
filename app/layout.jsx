59b78a06a0c1d94c172f80445df3c02fb5c7d97d
realmaxy <max_sem777@mail.ru>
2023-05-30 18:00:08 +0300

    some meta changes

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,5 @@
 import Header from '@/components/Header/Header'
 import '../sass/globals.scss'
-import Head from 'next/head'
 
 export const metadata = {
   title: 'Кубань Мебель — Производство качественной мебели по ценам фабрики.',
@@ -10,19 +9,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <Head>
-        <link rel='shortcut icon' href='../public/favicon.ico'/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://next-furniture-ten.vercel.app/"/>
-        <meta property="og:title" content="Кубань Мебель — Производство качественной мебели по ценам фабрики."/>
-        <meta property="og:description" content="Качественная мебель для дома и офиса в Краснодаре. Широкий выбор диванов, кроватей, столов и другой мебели по ценам фабрики."/>
-        <meta property="og:image" content=""/>
-        <meta property="twitter:card" content="summary_large_image"/>
-        <meta property="twitter:url" content="https://next-furniture-ten.vercel.app/"/>
-        <meta property="twitter:title" content="Кубань Мебель — Производство качественной мебели по ценам фабрики."/>
-        <meta property="twitter:description" content="Качественная мебель для дома и офиса в Краснодаре. Широкий выбор диванов, кроватей, столов и другой мебели по ценам фабрики."/>
-        <meta property="twitter:image" content=""/>
-      </Head>
       <body>
         <Header/>
         {children}
