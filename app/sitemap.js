296f3d2f9dff6f33528a6983e392a058f642bc2a
realmaxy <max_sem777@mail.ru>
2023-06-02 17:48:16 +0300

    change: sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,4 +1,4 @@
-import { cards } from "./products/items"
+import { categories, livingrooms, bedrooms, offices, kitchens, hallways } from "./products/data"
 
 export default async function sitemap() {
   const routes = ['', 'about', 'products'].map(route => ({
@@ -6,10 +6,35 @@ export default async function sitemap() {
     lastModified: new Date().toISOString()
   }))
 
-  const products = cards.map((product)=> ({
-    url: `http://localhost3000:/products/${product.id}`,
+  const categoriesPages = categories.map((category)=> ({
+    url: `http://localhost3000:/products/${category.id}`,
+    lastModified: new Date().toISOString()
+  }))
+ 
+  const livingroomsPages = livingrooms.map((item) => ({
+    url: `http://localhost3000:/products/${item.category}/${item.id}`,
     lastModified: new Date().toISOString()
   }))
 
-  return [...routes, ...products]
+  const bedroomsPages = bedrooms.map((item) => ({
+    url: `http://localhost3000:/products/${item.category}/${item.id}`,
+    lastModified: new Date().toISOString()
+  }))
+
+  const officesPages = offices.map((item) => ({
+    url: `http://localhost3000:/products/${item.category}/${item.id}`,
+    lastModified: new Date().toISOString()
+  }))
+
+  const kitchensPages = kitchens.map((item) => ({
+    url: `http://localhost3000:/products/${item.category}/${item.id}`,
+    lastModified: new Date().toISOString()
+  }))
+
+  const hallwaysPages = hallways.map((item) => ({
+    url: `http://localhost3000:/products/${item.category}/${item.id}`,
+    lastModified: new Date().toISOString()
+  }))
+
+  return [...routes, ...categoriesPages, ...livingroomsPages, ...bedroomsPages, ...kitchensPages, ...officesPages, ...hallwaysPages ]
 }
